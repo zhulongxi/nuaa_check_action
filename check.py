4999ba99116f41f9216c41596eb18c5cfb32ff54
zhulongxi <48077103+zhulongxi@users.noreply.github.com>
2022-11-24 10:07:46 +0800

    Update check.py

--- check.py
+++ check.py
@@ -32,7 +32,7 @@ def login(stu_number, password):
             response = requests.get('https://m.nuaa.edu.cn/a_nuaa/api/login-v2/index', cookies=cookies,
                              data='username={}&password={}&grant_type=password&imei={}&mobile_type={}&scope=&sid={}'.format(login_id,
                                                                                                  login_password,"158916bc3d341bf9",
-                                                                                                 android,"158916bc3d341bf9"))
+                                                                                                 "android","158916bc3d341bf9"))
             print('login...:', response.status_code)
 
             # cookies2 = response.headers['Set-Cookie']
